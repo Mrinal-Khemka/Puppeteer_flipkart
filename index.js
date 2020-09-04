b5c24bb01b0c0d721a567a6c7d8d81257c223b9b
Mrinal Khemka <57214057+Mrinal-Khemka@users.noreply.github.com>
2020-09-04 14:32:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const flipkart_URL='https://www.flipkart.com/poco-m2-pro-green-greener-64-gb/p/itm5de3b6eb57ab4?pid=MOBFT7MKFHTAPDYD&lid=LSTMOBFT7MKFHTAPDYDLRJC1B&marketplace=FLIPKART&srno=s_1_7&otracker=search&otracker1=search&fm=SEARCH&iid=7498679b-ae7f-48d1-a775-4376731fe28b.MOBFT7MKFHTAPDYD.SEARCH&ppt=sp&ppn=sp&ssid=q2g0ggpogw0000001599051475381&qH=b83349d0f5338c62';
+    const flipkart_URL='https://www.flipkart.com/realme-x2-pearl-green-64-gb/p/itm75023903eb431';
     // const browser = await puppeteer.launch({
     //   headless: false,
     //   slowMo: 400
